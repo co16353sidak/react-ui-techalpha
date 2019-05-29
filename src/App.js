5b731482fb286e0e7ca30bf5afc319f7b556f3ba
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-05-29 22:39:04 +0530

    inital setup after bootstrapping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function TableRow(props) {
+  return (
+    <tr>
+      <td>Sample</td>
+      <td>Sample</td>
+      <td>Sample</td>
+    </tr>
+  )
+}
+
+function Table(props) {
+    return (
+      <div className = "table-wrapper">
+        <h1> {props.name} </h1>
+        <h3>Total = {props.total}</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Sum(BTC)</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableRow />
+            <TableRow />
+            <TableRow />
+            <TableRow />
+          </tbody>
+        </table>
+      </div>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Table name="Selling BTC" total="25000 BTC"/>
+      <Table name="Buying BTC" total="0.01555923 BTC"/>
     </div>
   );
 }
