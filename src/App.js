0a4589d4c6477d1c7545ce324f7dd938434c6b13
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-05-30 01:19:48 +0530

    interim itteration

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function TableRow(props) {
     <tr>
       <td>Sample</td>
       <td>Sample</td>
-      <td>Sample</td>
+      <td className="last">Sample</td>
     </tr>
   )
 }
 
 function Table(props) {
     return (
-      <div className = "table-wrapper">
+      <div className = { props.class }>
         <h1> {props.name} </h1>
         <h3>Total = {props.total}</h3>
-        <table className="table">
+        <table>
           <thead>
             <tr>
               <th>Sum(BTC)</th>
@@ -35,13 +35,31 @@ function Table(props) {
     );
 }
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Table name="Selling BTC" total="25000 BTC"/>
-      <Table name="Buying BTC" total="0.01555923 BTC"/>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ["Buying BTC", "Selling BTC"]
+    };
+  }
+  render() {
+
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <div id="table1" className="table1">
+            <Table name={this.state.name[0]} total="25000 AUD" class="buy" />
+          </div>
+          <div className="table2">
+            <Table name={this.state.name[1]} total="0.01555923 BTC" class="sell" />
+          </div>
+        </div>
+        <button className="switch" >
+          Switch          
+        </button>
+      </div>
+      );
+    }
+  }
 
 export default App;
